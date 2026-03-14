3ac80f51cd4b78852aee7667600e82e59d5843fd
Codrut <petculescucodrut@gmail.com>
2026-03-14 22:11:32 +0200

    Wowiie

--- Main/paper-fun-rpg/UI/overlay/layer_overlay.gd
+++ Main/paper-fun-rpg/UI/overlay/layer_overlay.gd
@@ -1,4 +1,4 @@
-extends TextureButton
+extends CanvasLayer
 
 
 # Called when the node enters the scene tree for the first time.
@@ -10,9 +10,18 @@ func _ready() -> void:
 func _process(delta: float) -> void:
 	pass
 
-func _on_button_pressed():
+
+func _on_button_help_pressed() -> void:
 	# Your button logic here
 	print("HELP Clicked! 🍦")
+	
+	# This stops the input from reaching the game world
+	get_viewport().set_input_as_handled()
+
+
+func _on_button_pause_pressed() -> void:
+	# Your button logic here
+	print("PAUSE Clicked! 🍦")
 	
 	# This stops the input from reaching the game world
 	get_viewport().set_input_as_handled()
