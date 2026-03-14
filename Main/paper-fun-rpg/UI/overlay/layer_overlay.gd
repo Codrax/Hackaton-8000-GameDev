452fa7a6646168f20aa85fc92cfd0f2b9d06d60e
Sasukappa <oritochun@gmail.com>
2026-03-14 23:16:23 +0200

    Auto stash before merge of "main" and "origin/main"

--- Main/paper-fun-rpg/UI/overlay/layer_overlay.gd
+++ Main/paper-fun-rpg/UI/overlay/layer_overlay.gd
@@ -11,15 +11,10 @@ func _on_pressed() -> void:
 
 
 func _on_button_help_pressed() -> void:
-	# Your button logic here
 	print("HELP Clicked! 🍦")
-	
-	# This stops the input from reaching the game world
 	get_viewport().set_input_as_handled()
 
-
 func _on_button_pause_pressed() -> void:
-	# Your button logic here
 	if pause_menu == null:
 		print("ERROR NOT FOUND")
 		push_error("Pause_Menu nu a fost gasit.")
