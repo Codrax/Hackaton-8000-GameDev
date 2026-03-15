8644cf523faf67c83552652130e74219b68877e2
Codrut <petculescucodrut@gmail.com>
2026-03-15 15:58:50 +0200

    Http

--- Main/paper-fun-rpg/UI/AI.gd
+++ Main/paper-fun-rpg/UI/AI.gd
@@ -1,7 +1,7 @@
 extends Node
 
 const API_URL = "https://devsocket.025555.xyz/"
-const API_URL_LOCAL = "localhost:8010"
+const API_URL_LOCAL = "http://localhost:8010"
 const AUTH_TOKEN = "b8d4de9d-4c35-4143-b0c4-61c3776c0586"
 
 # Local LLM
